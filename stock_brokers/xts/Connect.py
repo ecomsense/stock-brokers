2936177eb77d8b3ac72a8da2a7fe956607c0bdb8
pannet1 <prog@ecomsense.in>
2024-06-17 08:03:43 +0530

    hard code jainam url

--- stock_brokers/xts/Connect.py
+++ stock_brokers/xts/Connect.py
@@ -38,7 +38,7 @@ class XTSConnect(XTSCommon):
     """Get the configurations from config.ini"""
 
     disable_ssl = True
-    root = "http://jtrade.jainam.in"
+    root = "http://jtrade.jainam.in:5000"
     broadcastMode = "Full"
     # Default root API endpoint. It's possible to
     # override this by passing the `root` parameter during initialisation.
